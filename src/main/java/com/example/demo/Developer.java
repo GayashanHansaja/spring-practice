298b7df517d689f25b637afa457f6534ed57f32a
Gayashan <hansagayashan7@gmail.com>
2025-05-21 23:14:57 +0530

    test 3 type of dependency injection

--- src/main/java/com/example/demo/Developer.java
+++ src/main/java/com/example/demo/Developer.java
@@ -1,11 +1,28 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Developer {
 
+    // field injection
+    //@Autowired
+    private Vscode vscode;
+
+    // constructor injection
+/*    public Developer(Vscode vscode){
+        this.vscode = vscode;
+    }*/
+
+    // setter injection
+    @Autowired
+    public void setVscode(Vscode vscode){
+        this.vscode = vscode;
+    }
+
     public void build(){
+        vscode.code();
         System.out.println("Building app");
     }
 }
