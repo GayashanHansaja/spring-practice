992abfdec0bd6033ac34ff4681dad30345199b9e
Gayashan <hansagayashan7@gmail.com>
2025-06-17 23:02:11 +0530

    create simple web app using spring boot

---
+++ simpleWebApp/src/main/java/org/example/simplewebapp/HomeController.java
@@ -0,0 +1,18 @@
+package org.example.simplewebapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping ("/")
+    /*@ResponseBody use this if you dont use @RestController*/
+    public String greet(){
+        return "Hello Gayashan!";
+    }
+    @RequestMapping ("/about")
+    public String about(){
+        return "This is a simple web application using Spring Boot.";
+    }
+}
