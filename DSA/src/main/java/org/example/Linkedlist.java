d06c0b57947329daca7f0710a43a6c730bf34275
Gayashan <hansagayashan7@gmail.com>
2025-07-29 21:36:47 +0530

    practice linked list

---
+++ DSA/src/main/java/org/example/Linkedlist.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.util.LinkedList;
+public class Linkedlist {
+ public static void main(String[] args) {
+     LinkedList<Integer> list = new LinkedList<>();
+
+     list.add(1);
+     list.add(2);
+     list.add(3);
+     list.addFirst(0);
+     list.addLast(4);
+
+     list.remove(0);
+
+     System.out.println(list);
+ }
+}
